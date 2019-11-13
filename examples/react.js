e9fd408fef4f38cbc33df807fb05c2b818615353
Remco Aalbregt <remcoaalbregt@gmail.com>
2019-11-13 08:59:06 +0100

    initial commit

---
+++ examples/react.js
@@ -0,0 +1,5 @@
+const Component = props => {
+  return <p>{props.text}</p>
+};
+
+render(<Component text="Hello World" />);
---
+++ ideas.md
@@ -0,0 +1,2 @@
+* Styling aanbieden
+* Zeer korte basis in Redux achteraf
